628afb77569d4ebd2f37f36150d3832b3e1f4f23
shoharto <shoharto@gmail.com>
2019-04-06 11:45:36 +0600

    arithmetic operator

---
+++ operators/arithmetic.php
@@ -0,0 +1,13 @@
+<?php
+
+  $number1 = 20;
+  $number2 = 15;
+
+
+   echo 'Sum:' . ($number1 + $number2) .'</br>';
+   echo 'Diff:' . ($number1 - $number2) .'</br>';
+   echo 'Mul:' . ($number1 * $number2) .'</br>';
+   echo 'Div:' . ($number1 / $number2) .'</br>';
+   echo 'Mod:' . ($number1 % $number2) .'</br>';
+   echo 'Power:' . ($number1 ** $number2) .'</br>';
+?>
