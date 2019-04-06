2d2309fa267305df94f3ce97a5c4caf847d0e637
shoharto <shoharto@gmail.com>
2019-04-06 12:48:23 +0600

    comparison operators implement

---
+++ operators/comparison.php
@@ -0,0 +1,27 @@
+<?php
+
+  $number1 = 10;
+  $number2 = 20;
+
+    /* Equal operator */
+  var_dump($number1 == $number2);
+
+     /* Identical operator */
+   var_dump($number1 === $number2);
+
+      /* Not equal operator */
+    var_dump($number1 != $number2);
+
+       /* Less than OR equal operator */
+    var_dump($number1 <= $number2);
+
+        /* Greater than or equal operator */
+    var_dump($number1 >= $number2);
+
+        /* Spaceshif operator */
+      var_dump($number1 <=> $number2);
+
+
+
+
+?>
