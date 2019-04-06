71c93f3c1a2d004c37548c2d9f9d927050d20b9a
shoharto <shoharto@gmail.com>
2019-04-06 11:22:58 +0600

    array operation

---
+++ array.php
@@ -0,0 +1,29 @@
+<?php
+
+  $persone = [
+    'first_name' => 'Fajle',
+    'last_name' => 'Rabbi',
+    'address' => [
+        'village' => 'Mathter vita',
+        'Thana' => 'Dhunat',
+        'District' => 'Bogura',
+        'country' =>[
+            'first' => 'Bangladesh',
+            'code' => 'BD'
+        ]
+        ]
+  ];
+
+  echo $persone['first_name'] .'</br>';
+  echo $persone['last_name'] .'</br>';
+  echo $persone['address']['village'] .'</br>';
+  echo $persone['address']['Thana'] .'</br>';
+  echo $persone['address']['District'] .'</br>';
+  echo $persone['address']['country']['first'] .'</br>';
+  echo $persone['address']['country']['code'] .'</br>';
+
+
+
+
+
+?>
